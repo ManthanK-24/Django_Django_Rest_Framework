aea0a16e539e532d0663f1a1fb6d1ac5451d8db0
ManthanK-24 <manthandec@gmail.com>
2026-03-30 18:03:19 +0530

    adding project

---
+++ README.md
@@ -0,0 +1,23 @@
+# Django Rest Framework 
+
+## Get Data API 
+
+![Get Method](./static/DRF_1.png)
+
+## Create Data API 
+![Create Method](./static/DRF_2.png) 
+
+## Data Insertion 
+![Data Inserted](./static/DRF_3.png)
+
+## Patch Data API 
+![Patch API](./static/DRF_4.png)
+
+## Updated Data After Patch
+![Updated Data](./static/DRF_5.png)
+
+## Delete Data API
+![Delete Data](./static/DRF_6.png)
+
+## Updated Data After Delete
+![Updated Data](./static/DRF_7.png)
---
+++ core/expense/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Transactions
+from .serializers import TransactionsSerializer
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+
+
+@api_view()
+def get_transactions(request):
+    queryset = Transactions.objects.all()
+    serializer = TransactionsSerializer(queryset, many=True)
+
+    return Response({
+        "data": serializer.data
+    })
+
+
+
+class TransactionAPI(APIView):
+    def get(self, request):
+        queryset = Transactions.objects.all()
+        serializer = TransactionsSerializer(queryset, many=True)
+        return Response({
+            "data":serializer.data
+        })
+    
+    def post(self, request):
+        data = request.data
+        serializer = TransactionsSerializer(data = data)
+        if not serializer.is_valid():
+            return Response({
+                "message": "problem in adding in transaction",
+                "errors": serializer.errors,
+            })
+        serializer.save()
+        return Response({
+            "message": "this is a post method",
+            "data": serializer.data 
+        })
+    
+    def put(self, request):
+        return Response({
+            "message": "this is a put method"
+        })
+    
+    def patch(self, request):
+        data = request.data
+
+        if not data.get('id'):
+             return Response({
+                "message": "problem in patching in transaction",
+                "errors": "id is required",
+            })
+        
+        transaction = Transactions.objects.get(id=data.get('id'))
+        serializer = TransactionsSerializer(transaction, data=data, partial=True)
+        if not serializer.is_valid():
+            return Response({
+                "message": "problem in adding in transaction",
+                "errors": serializer.errors,
+            })
+        serializer.save()
+        return Response({
+             "message": "this is a patch method",
+            "data": serializer.data 
+        })
+    
+    def delete(self, request):
+        data = request.data
+
+        if not data.get('id'):
+             return Response({
+                "message": "problem in patching in transaction",
+                "errors": "id is required",
+            })
+        
+        transaction = Transactions.objects.get(id=data.get('id')).delete()
+        
+        
+        return Response({
+             "message": "this is a delete method",
+            "data": {} 
+        })
